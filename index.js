52cd4a7ae924d6173d775e1091d6f099a11b9d08
Mikael Edebro <mikael.edebro@gmail.com>
2018-06-21 14:51:13 +0200

    Moer work

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' })
-})
-
 const PORT = process.env.PORT || 5000
+const keys = require('./config/keys')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+
+// mongoose config
+const mongoose = require('mongoose')
+mongoose.connect(keys.mongoURI)
+
+require('./models/user')
+
+// passport config
+require('./services/passport')
+
+const app = express()
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+
+require('./routes/authRoutes')(app)
+
 app.listen(PORT)
